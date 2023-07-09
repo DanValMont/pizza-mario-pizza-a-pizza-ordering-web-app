8d4c3fb6d8a54f0698508c8e4fc1a016acca66cb
Daniel Valdez Montalvan <danielvaldezmontalvan@gmail.com>
2023-07-09 14:53:30 -0400

    store.js - file changed admin: adminSlice to admin: adminReducer.

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartSlice";
-import adminSlice from "./adminSlice";
+import adminReducer from "./adminSlice";
 
 export default configureStore({
   reducer: {
     cart: cartReducer,
-    admin: adminSlice,
+    admin: adminReducer,
   },
 });
